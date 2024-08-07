9b95f52dd1cf8b633485c2e157a93d15911fde0c
Kimberly Jemide <kimberlyjemide@gmail.com>
2024-08-07 20:35:55 +0100

    Initial Commit

---
+++ Efficio.official/Efficio_officialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Efficio_officialApp.swift
+//  Efficio.official
+//
+//  Created by KJemide on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Efficio_officialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
