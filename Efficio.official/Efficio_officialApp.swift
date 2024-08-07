b7be60ba179f7c0c59dabd09b7d5201145d2064f
Kimberly Jemide <kimberlyjemide@gmail.com>
2024-08-07 21:05:07 +0100

    Testing connection to GitHub

--- Efficio.official/Efficio_officialApp.swift
+++ Efficio.official/Efficio_officialApp.swift
@@ -5,10 +5,14 @@
 //  Created by KJemide on 07/08/2024.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct Efficio_officialApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Efficio.official/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  Efficio.official
+//
+//  Created by KJemide on 07/08/2024.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
