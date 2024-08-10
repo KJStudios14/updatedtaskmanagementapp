9ea347cdca4133b70d41a8b974239097fff1742b
Kimberly Jemide <kimberlyjemide@gmail.com>
2024-08-10 14:30:28 +0100

    Initial Commit

---
+++ theofficialefficioapp/theofficialefficioappApp.swift
@@ -0,0 +1,17 @@
+//
+//  theofficialefficioappApp.swift
+//  theofficialefficioapp
+//
+//  Created by KJemide on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct theofficialefficioappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
