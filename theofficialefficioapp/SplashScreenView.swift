c12290150878dada82cbcd6411f38f2376002ae2
Kimberly Jemide <kimberlyjemide@gmail.com>
2024-08-11 09:59:54 +0100

    Creating the Splash Screen and adding the app icon

--- theofficialefficioapp/SplashScreenView.swift
+++ theofficialefficioapp/SplashScreenView.swift
@@ -1,5 +1,5 @@
 //
-//  SplashScreenView.swift
+//  GettingStartedView.swift
 //  theofficialefficioapp
 //
 //  Created by KJemide on 10/08/2024.
@@ -9,10 +9,25 @@ import SwiftUI
 
 struct SplashScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.efficioblue)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Image("logodark")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+                    .padding(.bottom, 140)
+                
+                Spacer()
+            }
+        }
     }
 }
+    #Preview {
+        SplashScreenView()
+    }
 
-#Preview {
-    SplashScreenView()
-}
--- theofficialefficioapp/theofficialefficioappApp.swift
+++ theofficialefficioapp/theofficialefficioappApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct theofficialefficioappApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
